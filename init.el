54272a53b97c55e6e9b2ebf6cddb3a59eb1348aa
Martin Rykfors <martinrykfors@gmail.com>
2014-11-21 21:39:02 +0000

    make processing mode a required package

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
                      aggressive-indent
                      narrow-indirect
                      expand-region
-                     evil-surround))
+                     evil-surround
+                     processing-mode))
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
