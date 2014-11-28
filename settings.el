b3d6a081e5725dc2772eeb35477ee665a2d32b4f
Martin Rykfors <martinrykfors@gmail.com>
2014-11-28 22:30:15 +0000

    add interactive functions for toggling sound

--- settings.el
+++ settings.el
@@ -200,4 +200,18 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (unless (and (fboundp 'play-sound-internal)
              (subrp (symbol-function 'play-sound-internal)))
   (require 'play-sound))
-(add-hook 'evil-insert-state-exit-hook (lambda () (play-sound-file "~/.emacs.d/sounds/204466__nhumphrey__typewriter-ding_cropped.wav")))
+(setq ryk-typewriter-ding nil)
+(add-hook 'evil-insert-state-exit-hook
+          (lambda ()
+            (if ryk-typewriter-ding
+                (play-sound-file "~/.emacs.d/sounds/204466__nhumphrey__typewriter-ding_cropped.wav"))))
+(defun ryk-typewriter ()
+  (interactive)
+  (if ryk-typewriter-ding
+      (progn
+        (setq ryk-typewriter-ding nil)
+        (princ "old-timey hacking disabled"))
+    (progn
+      (setq ryk-typewriter-ding t)
+      (princ "old-timey hacking enabled"))))
+(global-set-key (kbd "<f3>") 'ryk-typewriter)
